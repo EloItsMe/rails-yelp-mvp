1dd1d31d0edc219441e805dd54b0db1d5d1a3aae
Elowan Audouin-le Guyader <elowan.aud@proton.me>
2023-05-24 12:48:11 +0200

    AddNewAndCreatePage

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,9 +8,24 @@ class RestaurantsController < ApplicationController
   def show
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
   private
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :category, :address, :phone_number)
   end
 end
