e38679c63292e0ab18edf9b1c020f4d4c6d5e6ce
Elowan Audouin-le Guyader <elowan.aud@proton.me>
2023-05-24 11:50:45 +0200

    BetterSyntax

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,5 +6,5 @@ class Restaurant < ApplicationRecord
   validates :phone_number, presence: true
   validates :category, presence: true
   validates :category, inclusion: { in: %w[chinese italian japanese french belgian],
-                                    message: "%{value} is not a valid size" }
+                                    message: '%<value> is not a valid size' }
 end
